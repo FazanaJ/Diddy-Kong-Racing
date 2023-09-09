350b9949b68c2ec935c15e7233b3dc64162a8ca9
Fazana <52551480+FazanaJ@users.noreply.github.com>
2023-09-09 20:33:45 +0100

    Update on_build.sh

--- on_build.sh
+++ on_build.sh
@@ -2,4 +2,4 @@
 # $1 = Output directory (build/us_1.0)
 
 # Take the first megabyte of the rom file, and copy it to a new file called "dkr_code.bin"
-head -c 1048576 $1/dkr.z64 > $1/dkr_code.bin 
+head -c 1048576 $1/dkr.z64 > $1/dkr_code.bin
