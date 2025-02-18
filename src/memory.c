ab45b3ca6047306089b459ace6aeecd245302fea
Fazana <52551480+FazanaJ@users.noreply.github.com>
2025-02-18 09:59:52 +0000

    brug

--- src/memory.c
+++ src/memory.c
@@ -174,7 +174,7 @@ MemoryPoolSlot *mempool_slot_find(MemoryPools poolIndex, s32 size, u32 colourTag
     pool = &gMemoryPools[poolIndex];
     if ((pool->curNumSlots + 1) == (*pool).maxNumSlots) {
         interrupts_enable(intFlags);
-        puppyprint_assert("Out of slots in pool %d. (%X)\nSP: %X", poolIndex, puppyprint_colourtag(colourTag), stack_pointer());
+        puppyprint_assert("Out of slots in pool %d. (%X)\nSP: %X", poolIndex, puppyprint_colourtag(colourTag), stack_pointer()->sp);
         puppyprint_load_snapshot(PP_LOAD_MALLOC, profiler_get_timer());
         return NULL;
     }
@@ -203,7 +203,7 @@ MemoryPoolSlot *mempool_slot_find(MemoryPools poolIndex, s32 size, u32 colourTag
     }
     interrupts_enable(intFlags);
 
-    puppyprint_log(LOG_ERROR, "Allocation failed (%X %s) SP: %X\n", colourTag, sPuppyprintMemColours[colourTag], stack_pointer());
+    puppyprint_log(LOG_ERROR, "Allocation failed (%X %s) SP: %X\n", colourTag, sPuppyprintMemColours[colourTag], stack_pointer()->sp);
     
     puppyprint_load_snapshot(PP_LOAD_MALLOC, profiler_get_timer());
     return NULL;
