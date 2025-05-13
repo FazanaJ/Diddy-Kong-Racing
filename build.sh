2aeebfe682b12c3821b61c776d5584f6f7097650
Fazana <52551480+FazanaJ@users.noreply.github.com>
2025-05-13 22:07:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+if [ "$(uname)" = "Darwin" ]; then
+python3 ./tools/python/map_dummy.py; gmake assets -j; python3 ./tools/python/map_gen.py ./build/dkr.us.v77.map ./assets/map_symbols.bin; gmake assets -j
+else
 python3 ./tools/python/map_dummy.py; make assets -j; python3 ./tools/python/map_gen.py ./build/dkr.us.v77.map ./assets/map_symbols.bin; make assets -j
+fi
