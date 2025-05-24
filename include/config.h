17cf0a34c8bd1238248a61765dc277a39b11bedc
Fazana <52551480+FazanaJ@users.noreply.github.com>
2025-05-24 17:06:59 +0100

    d

--- include/config.h
+++ include/config.h
@@ -6,14 +6,14 @@
 
 #define SKIP_TITLE 1
 
-#define DEBUG
+//#define DEBUG
 
 /* -------------------------Memory------------------------ */
 #define EXPANSION_PAK_SUPPORT 0  // Allow the game to use the expansion pak if it's inserted. 0 Means never use, 1 means use if available, 2 means disallow 4MB
 //#define FORCE_4MB_MEMORY  // Force the game to use 4MB of memory, even if an expansion pak is inserted.
 
 #define UNLOCK_ALL
-#define SKIP_INTRO SKIP_TITLE
+//#define SKIP_INTRO SKIP_TITLE
 #define AUTOPLAY_DEFAULT AUTOPLAY_OFF
 //#define SKIP_NEW_GAME
 //#define OPEN_ALL_DOORS
--- src/autoplay.c
+++ src/autoplay.c
@@ -6,8 +6,6 @@
 #include "printf.h"
 #include "thread30_bgload.h"
 
-#ifdef DEBUG
-
 extern s32 gCurrentMenuId;
 extern s8 gDoneTalkingToNPC[];
 extern s32 sLogicUpdateRate;
